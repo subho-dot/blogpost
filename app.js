95f4ed5fd9d32d3a5b091c816ea50049a4d45532
Subhodip Sadhu <84008971+subho-dot@users.noreply.github.com>
2023-02-05 00:27:50 +0530

    Update app.js
    port change

--- app.js
+++ app.js
@@ -105,7 +105,5 @@ app.get('/posts/:postId', (req, res) => {
     
 });
 
-
-app.listen(process.env.PORT || 3000 , function() {
-  console.log("Server started on port 3000");
-});
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
